bfe24bb81366284f7d9977960980f32e8f11e83a
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2023-07-17 15:30:48 +0200

    fix: missing types
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- modules/versions.nix
+++ modules/versions.nix
@@ -8,7 +8,7 @@ in
     enable = lib.mkEnableOption "Print versions when entering a shell";
 
     exec = lib.mkOption {
-      type = types.lines;
+      type = lib.types.lines;
       description = "Bash code to execute to print version information.";
       default = "";
     };
